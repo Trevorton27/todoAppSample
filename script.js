ba6309cead85040295c8f5601a27ebae32c60088
Angel Prado <angel_joaquin12@outlook.com>
2022-05-08 22:37:39 -0500

    buttons work and added css

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-// Javascript needed to input todo item- done
-// javascript needed to submit item- done
+let addButton = document.querySelector(".submitButton");
+addButton.addEventListener("click", showInputItemOnPage);
 
-// html needed to show list of submitted items-done
-
-// javascript needed to check off todo items and mark them as completed using strikethrough font- done
-
-// javascript needed to delete existing tofo item (for those that have been marked as completed)
-
-let addTodoButton = document.querySelector(".submitButton");
-addTodoButton.addEventListener("click", showTodo);
-
-function showTodo(event) {
+function showInputItemOnPage(event) {
   const inputElement = document.getElementById("taskBox");
   const text = inputElement.value;
   const fullEntry = createTodo(text);
@@ -19,24 +10,36 @@ function showTodo(event) {
   toDoListedItems.append(todo);
   inputElement.value = "";
 }
-
 function createTodo(text) {
   const completedButton = document.createElement("button");
   completedButton.textContent = "o";
-  completedButton.addEventListener("click", function () {
-    listedItemToDo.innerHTML =
-      "<strike>" + listedItemToDo.innerHTML + "</strike>";
+  completedButton.style.color = "white";
+  completedButton.style.backgroundColor = "blank";
+  completedButton.style.borderRadius = "40px";
+  completedButton.value = 0;
+  console.log(completedButton);
+  completedButton.addEventListener("click", function (completeButton) {
+    if (completedButton.value == 0) {
+      completedButton.value = 1;
+      listedItemToDo.style.textDecoration = "line-through";
+      deleteButton.disabled = false;
+    } else {
+      completedButton.value = 0;
+      listedItemToDo.style.textDecoration = "none";
+      deleteButton.disabled = true;
+    }
   });
-
   const deleteButton = document.createElement("button");
   deleteButton.textContent = "X";
   deleteButton.style.color = "white";
   deleteButton.style.backgroundColor = "red";
+  deleteButton.style.borderRadius = "40px";
+  deleteButton.disabled = true;
   deleteButton.addEventListener("click", function () {
-    console.log("Delete was clicked");
     listedItemToDo.remove();
   });
   const listedItemToDo = document.createElement("li");
+  listedItemToDo.style.paddingInline = "20px";
   listedItemToDo.style.listStyle = "none";
   listedItemToDo.append(completedButton);
   listedItemToDo.append(text);
